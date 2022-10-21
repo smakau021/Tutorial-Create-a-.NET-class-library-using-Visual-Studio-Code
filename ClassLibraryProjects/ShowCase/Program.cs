1393852b6d2a172218be62118f5edf6b3889cec2
Sphelele Makau <smakau021@student.wethinkcode.co.za>
2022-10-21 08:13:43 +0200

    I really enjoyed creating a class libriry, adding it to the solution , adding console app and referencing it to use the library and run the program

---
+++ ClassLibraryProjects/ShowCase/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// The code uses the row variable to maintain a count of the number 
+// of rows of data written to the console window. Whenever it's greater
+//than or equal to 25, the code clears the console window and displays a message to the user.
+
+// The program prompts the user to enter a string. It indicates whether 
+// the string starts with an uppercase character. If the user presses the
+// Enter key without entering a string, the application ends, and the console window closes.
+// 
+using UtilityLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int row = 0;
+
+        do
+        {
+            if (row == 0 || row >= 25)
+                ResetConsole();
+
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)) break;
+            Console.WriteLine($"Input: {input}");
+            Console.WriteLine("Begins with uppercase? " +
+                 $"{(input.StartsWithUpper() ? "Yes" : "No")}");
+            Console.WriteLine();
+            row += 4;
+        } while (true);
+        return;
+
+        // Declare a ResetConsole local method
+        void ResetConsole()
+        {
+            if (row > 0)
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine($"{Environment.NewLine}Press <Enter> only to exit; otherwise, enter a string and press <Enter>:{Environment.NewLine}");
+            row = 3;
+        }
+    }
+}
