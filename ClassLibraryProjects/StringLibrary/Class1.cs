d5bb3a32a5327967fcca6da902a9307352510b19
Sphelele Makau <smakau021@student.wethinkcode.co.za>
2022-10-21 08:41:03 +0200

    added comments explaning the libriary class or utility

--- ClassLibraryProjects/StringLibrary/Class1.cs
+++ ClassLibraryProjects/StringLibrary/Class1.cs
@@ -1,4 +1,15 @@
-﻿namespace UtilityLibraries;
+﻿// The class library, UtilityLibraries.StringLibrary, contains a method
+// named StartsWithUpper. This method returns a Boolean value that indicates
+// whether the current string instance begins with an uppercase character. 
+// The Unicode standard distinguishes uppercase characters from lowercase characters.
+// The Char.IsUpper(Char) method returns true if a character is uppercase.
+
+// StartsWithUpper is implemented as an extension method so that you can call
+// it as if it were a member of the String class.
+
+
+
+namespace UtilityLibraries;
 
 public static class StringLibrary
 {
